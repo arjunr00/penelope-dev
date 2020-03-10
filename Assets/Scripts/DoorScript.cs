3b378fb53e584473a76714110f7dcf866dbfccb9
Sanketh Hegde <san2heg@gmail.com>
2020-03-10 03:52:11 -0700

    More door functionality

--- Assets/Scripts/DoorScript.cs
+++ Assets/Scripts/DoorScript.cs
@@ -5,16 +5,34 @@ using UnityEngine;
 public class DoorScript : MonoBehaviour
 {
     public bool isOpen = true;
+    public float rotSpeed = 1.0f;
+    Quaternion startRot, endRot;
+
+    private void Start()
+    {
+        startRot = Quaternion.LookRotation(transform.forward);
+        endRot = Quaternion.LookRotation(transform.right);
+    }
+
+    private void Update()
+    {
+        if (transform.rotation != endRot)
+        {
+            transform.rotation = Quaternion.Slerp(startRot, endRot, Time.time * rotSpeed);
+        }
+    }
 
     public void ToggleDoor()
     {
         print("toggling door");
         if (isOpen)
         {
-            transform.Rotate(0, 90, 0);
+            startRot = Quaternion.LookRotation(transform.forward);
+            endRot = Quaternion.LookRotation(transform.right);
         } else
         {
-            transform.Rotate(0, -90, 0);
+            endRot = Quaternion.LookRotation(transform.forward);
+            startRot = Quaternion.LookRotation(transform.right);
         }
         isOpen = !isOpen;
     }
